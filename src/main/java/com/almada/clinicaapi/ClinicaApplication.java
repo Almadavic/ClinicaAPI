87508a495bf5f09c883a98f13ba407c30f400235
Victor Almada <85299065+Almadavic@users.noreply.github.com>
2023-08-30 23:16:02 -0300

    Estruturando a arquitetura do projeto e ponto inicial

--- src/main/java/com/almada/clinicaapi/ClinicaApplication.java
+++ src/main/java/com/almada/clinicaapi/ClinicaApplication.java
@@ -2,7 +2,11 @@ package com.almada.clinicaapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport
+@EnableCaching
 @SpringBootApplication
 public class ClinicaApplication {
 
