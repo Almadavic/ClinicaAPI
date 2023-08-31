f3bdfdf7d4e4e5c34def588db7848796a7731f6c
Victor Almada <85299065+Almadavic@users.noreply.github.com>
2023-08-30 23:12:33 -0300

    Dependencias externas adicionadas

--- src/main/java/com/almada/clinicaapi/ClinicaApplication.java
+++ src/main/java/com/almada/clinicaapi/ClinicaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClinicaApiApplication {
+public class ClinicaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClinicaApiApplication.class, args);
+		SpringApplication.run(ClinicaApplication.class, args);
 	}
 
 }
