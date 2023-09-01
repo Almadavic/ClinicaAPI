0680fb2f2def81fa280420c669e108bf0dd89314
Victor Almada <85299065+Almadavic@users.noreply.github.com>
2023-09-01 02:44:43 -0300

    Adicionados testes relacionados ao verifyInstance do userAreaService

---
+++ src/test/java/com/almada/clinicaapi/service/serviceAction/userAreaService/VerifyInstanceMyProfile.java
@@ -0,0 +1,84 @@
+package com.almada.clinicaapi.service.serviceAction.userAreaService;
+
+import com.almada.clinicaapi.controller.UserAreaController;
+import com.almada.clinicaapi.dto.response.DentistResponseDTO;
+import com.almada.clinicaapi.dto.response.PatientResponseDTO;
+import com.almada.clinicaapi.dto.response.SecretaryResponseDTO;
+import com.almada.clinicaapi.dto.response.UserResponseDTO;
+import com.almada.clinicaapi.factory.DentistFactory;
+import com.almada.clinicaapi.factory.PatientFactory;
+import com.almada.clinicaapi.factory.SecretaryFactory;
+import com.almada.clinicaapi.factory.UserFactory;
+import com.almada.clinicaapi.service.serviceAction.UserAreaService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@ActiveProfiles(value = "test")
+@SpringBootTest
+class VerifyInstanceMyProfile {
+
+    @Autowired
+    private UserAreaService userAreaService;
+
+    @Autowired
+    private UserFactory userFactory;
+
+    @Autowired
+    private DentistFactory dentistFactory;
+
+    @Autowired
+    private PatientFactory patientFactory;
+
+    @Autowired
+    private SecretaryFactory secretaryFactory;
+
+    @Test
+    void verifySecretaryInstance() {
+
+        UserResponseDTO userResponseDTO = userAreaService.myProfile(secretaryFactory.entity());
+        Assertions.assertDoesNotThrow(() -> (SecretaryResponseDTO) userResponseDTO);
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/myprofile>;rel=\"self\""));
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/secretary>;rel=\"UPDATE\""));
+
+    }
+
+    @Test
+    void verifyDentistInstance() {
+
+        UserResponseDTO userResponseDTO = userAreaService.myProfile(dentistFactory.entity());
+        Assertions.assertDoesNotThrow(() -> (DentistResponseDTO) userResponseDTO);
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/myprofile>;rel=\"self\""));
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/dentist>;rel=\"UPDATE\""));
+
+    }
+
+    @Test
+    void verifyPatientInstance() {
+
+        UserResponseDTO userResponseDTO = userAreaService.myProfile(patientFactory.entity());
+        Assertions.assertDoesNotThrow(() -> (PatientResponseDTO) userResponseDTO);
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/myprofile>;rel=\"self\""));
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/patient>;rel=\"UPDATE\""));
+
+    }
+
+    @Test
+    void verifyGenericInstance() {
+
+        UserResponseDTO userResponseDTO = userAreaService.myProfile(userFactory.entity());
+        Assertions.assertFalse(userResponseDTO instanceof SecretaryResponseDTO);
+        Assertions.assertFalse(userResponseDTO instanceof DentistResponseDTO);
+        Assertions.assertFalse(userResponseDTO instanceof PatientResponseDTO);
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/myprofile>;rel=\"self\""));
+        Assertions.assertTrue(userResponseDTO.toString().contains("<http://localhost/userarea/admin>;rel=\"UPDATE\""));
+
+    }
+
+}
